4278a15a4f9097f35ebc1ce42ae18a26b99b0868
William Ramsey <wramsey@gmail.com>
2018-05-16 20:25:04 -0700

    Fetch first address from server

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log('campaigns', campaigns);
+
+    return { campaigns };
   }
 
   render() {
-    return <div>Campaigns Index</div>;
+    return <div>First Address: {this.props.campaigns[0]}</div>;
   }
 }
 
