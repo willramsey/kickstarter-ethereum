ec95184a16e8a4fce8c3809dcc450c795ae276b7
William Ramsey <wramsey@gmail.com>
2018-05-20 15:17:28 -0700

    Create RequestsNew component

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 class RequestIndex extends Component {
+  static async getInitialProps(props) {
+    return { address: props.query.address };
+  }
   render() {
-    return <h3>Request List</h3>;
+    const { address } = this.props;
+
+    return (
+      <Layout>
+        <h3>Requests</h3>
+        <Link route={`/campaigns/${address}/requests/new`}>
+          <a>
+            <Button primary>Add Request</Button>
+          </a>
+        </Link>
+      </Layout>
+    );
   }
 }
 
---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class RequestNew extends Component {
+  render() {
+    return <h3>New Request</h3>;
+  }
+}
+
+export default RequestNew;
