ef98c8735be0451584a12a4183bb1eb29afde85d
William Ramsey <wramsey@gmail.com>
2018-05-19 22:13:09 -0700

    Create Links for each campaign

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import factory from '../ethereum/factory';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -14,7 +15,11 @@ class CampaignIndex extends Component {
     const items = this.props.campaigns.map(address => {
       return {
         header: address,
-        description: <a>View Campaign</a>,
+        description: (
+          <Link route={`/campaigns/${address}`}>
+            <a>View Campaign</a>
+          </Link>
+        ),
         fluid: true,
       };
     });
@@ -27,13 +32,19 @@ class CampaignIndex extends Component {
       <Layout>
         <div>
           <h3>Open Campaigns</h3>
+
+          <Link route="/campaigns/new">
+            <a>
+              <Button
+                content="Create Campaign"
+                icon="add circle"
+                primary
+                floated="right"
+              />
+            </a>
+          </Link>
+
           {this.renderCampaigns()}
-          <Button
-            content="Create Campaign"
-            icon="add circle"
-            primary
-            floated="right"
-          />
         </div>
       </Layout>
     );
