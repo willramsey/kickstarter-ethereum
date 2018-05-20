bb095f273546fa3440362933ba590a61286a1861
William Ramsey <wramsey@gmail.com>
2018-05-20 16:13:02 -0700

    Add ability to create new requests

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -6,20 +6,54 @@ import web3 from '../../../ethereum/web3';
 import { Link, Router } from '../../../routes';
 
 class RequestNew extends Component {
-  state = { value: '', description: '', recipient: '' };
+  state = {
+    value: '',
+    description: '',
+    recipient: '',
+    isLoading: false,
+    errorMessage: '',
+  };
 
   static async getInitialProps(props) {
     return { address: props.query.address };
   }
 
-  render() {
+  handleSubmit = async event => {
+    event.preventDefault();
+
     const { value, description, recipient } = this.state;
+    const { address } = this.props;
+
+    const campaign = getCampaign(address);
+
+    this.setState({ isLoading: true, errorMessage: '' });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods
+        .createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
+        .send({ from: accounts[0] });
+
+      Router.pushRoute(`/campaigns/${address}/requests`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message.split('\n')[0] });
+    }
+    this.setState({ isLoading: false });
+  };
+
+  render() {
+    const {
+      value,
+      description,
+      recipient,
+      isLoading,
+      errorMessage,
+    } = this.state;
     const { address } = this.props;
 
     return (
       <Layout>
         <h3>Create a Request</h3>
-        <Form>
+        <Form onSubmit={this.handleSubmit} error={!!errorMessage}>
           <Form.Field>
             <label>Description</label>
             <Input
@@ -48,7 +82,10 @@ class RequestNew extends Component {
             />
           </Form.Field>
 
-          <Button primary>Create!</Button>
+          <Message error header="Uh Oh!" content={errorMessage} />
+          <Button loading={isLoading} primary>
+            Create!
+          </Button>
         </Form>
       </Layout>
     );
