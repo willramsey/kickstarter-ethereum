f307c5af1c4783f74176da9753309846205e8768
William Ramsey <wramsey@gmail.com>
2018-05-19 23:06:30 -0700

    Create helper function to access campaign instances and use in getInitialProps to return summary data

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import campaignInstance from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
     const address = props.query.address;
-    console.log('address', address);
-    return { address };
+    const campaign = campaignInstance(address);
+    const summary = await campaign.methods.getSummary().call();
+
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      numRequests: summary[2],
+      numApprovers: summary[3],
+      managerAddress: summary[4],
+    };
   }
 
   render() {
