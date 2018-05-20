528f380a748cb4cb592b8fd9fd936c871e94e3a0
William Ramsey <wramsey@gmail.com>
2018-05-19 17:01:49 -0700

    Add Form behavior to submit a new campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
-  state = { minContribution: '' };
+  state = {
+    minContribution: '',
+    errorMessage: '',
+    isLoading: false,
+  };
 
   handleChange = event => {
     this.setState({ minContribution: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.setState({ isLoading: true, errorMessage: '' });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods.createCampaign(this.state.minContribution).send({
+        from: accounts[0],
+      });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.setState({ isLoading: false });
   };
 
   render() {
@@ -14,7 +34,7 @@ class CampaignNew extends Component {
       <Layout>
         <h1>Create a Campaign</h1>
 
-        <Form>
+        <Form onSubmit={this.handleSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -23,8 +43,11 @@ class CampaignNew extends Component {
               label="wei"
               labelPosition="right"
             />
-            <Button primary>Create</Button>
           </Form.Field>
+          <Message error header="Uh oh!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.isLoading}>
+            Create
+          </Button>
         </Form>
       </Layout>
     );
