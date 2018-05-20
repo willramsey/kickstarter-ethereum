a7eebdc1e068e6e6418aa9f4a8bf3041cc49b876
William Ramsey <wramsey@gmail.com>
2018-05-19 23:18:38 -0700

    Create managerAddress Card component for a campaign

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import campaignInstance from '../../ethereum/campaign';
 
@@ -17,10 +18,33 @@ class CampaignShow extends Component {
     };
   }
 
+  renderCards = () => {
+    const {
+      minimumContribution,
+      balance,
+      numRequests,
+      numApprovers,
+      managerAddress,
+    } = this.props;
+
+    const items = [
+      {
+        header: managerAddress,
+        meta: 'Address of Manager',
+        description:
+          'The manager is the person who created this campaign. They can create requests to withdraw funds.',
+        style: { overflowWrap: 'break-word' },
+      },
+    ];
+
+    return <Card.Group items={items} />;
+  };
+
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
